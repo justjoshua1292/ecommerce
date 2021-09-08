9714753f93c155c1ced8c501dca7e5b8bdf08994
Joshua Harris <Justjoshua1292@gmail.com>
2021-09-08 18:30:29 -0400

    worked on category and product routes, on to tag routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,58 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product]
+  })
+  .then((results) => {
+    res.json(results)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  })
+  .then((results) => {
+    res.json(results)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then((results) => {
+    res.json(results)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((results) => {
+    res.json(results)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((results) => {
+    res.json(results)
+  })
 });
 
 module.exports = router;
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,15 +7,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  Product.findAll()
-  .then()
-  .catch()
+  Product.findAll({
+    include: [Category, {
+      model: Tag,
+      through: ProductTag
+    }]
+  })
+  .then((results) => {
+    res.json(results)
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      Category, 
+      {
+        model: Tag,
+        through: ProductTag
+    }]
+  })
+  .then((results) => {
+    res.json(results)
+  })
 });
 
 // create new product
@@ -94,6 +114,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((results) => {
+    res.json(results)
+  })
 });
 
 module.exports = router;
